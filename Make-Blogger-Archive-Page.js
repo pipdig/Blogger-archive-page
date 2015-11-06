045e580bfdb2ac338929554024cdec943e904d4a
pipdig <pipdig@users.noreply.github.com>
2015-11-06 18:33:58 +0000

    Update Make-Blogger-Archive-Page.js

--- Make-Blogger-Archive-Page.js
+++ Make-Blogger-Archive-Page.js
@@ -7,25 +7,25 @@ function LoadTheArchive(TotalFeed)
     var PostDays = new Array();
     if("entry" in TotalFeed.feed) 
     {
-	var PostEntries=TotalFeed.feed.entry.length;
-	for(var PostNum=0; PostNum<PostEntries ; PostNum++) 
-	{
-	    var ThisPost = TotalFeed.feed.entry[PostNum];
-	    PostTitles.push(ThisPost.title.$t);
-	    PostYears.push(ThisPost.published.$t.substring(0,4));
-	    PostMonths.push(ThisPost.published.$t.substring(5,7));
-	    PostDays.push(ThisPost.published.$t.substring(8,10));
-	    var ThisPostURL;
-	    for(var LinkNum=0; LinkNum < ThisPost.link.length; LinkNum++) 
-	    {
-		if(ThisPost.link[LinkNum].rel == "alternate") 
-		{
-		    ThisPostURL = ThisPost.link[LinkNum].href;
-		    break
-		}
-	    }
-	    PostURLs.push(ThisPostURL);
-	}
+ var PostEntries=TotalFeed.feed.entry.length;
+ for(var PostNum=0; PostNum<PostEntries ; PostNum++) 
+ {
+     var ThisPost = TotalFeed.feed.entry[PostNum];
+     PostTitles.push(ThisPost.title.$t);
+     PostYears.push(ThisPost.published.$t.substring(0,4));
+     PostMonths.push(ThisPost.published.$t.substring(5,7));
+     PostDays.push(ThisPost.published.$t.substring(8,10));
+     var ThisPostURL;
+     for(var LinkNum=0; LinkNum < ThisPost.link.length; LinkNum++) 
+     {
+  if(ThisPost.link[LinkNum].rel == "alternate") 
+  {
+      ThisPostURL = ThisPost.link[LinkNum].href;
+      break
+  }
+     }
+     PostURLs.push(ThisPostURL);
+ }
     }
     DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays);
 }
@@ -36,7 +36,7 @@ function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays)
     var NumberOfEntries=PostTitles.length;
     for(var EntryNum = 0; EntryNum < NumberOfEntries; EntryNum++)
     {
-	NameOfMonth = MonthNames[parseInt(PostMonths[EntryNum],10)-1]
-	document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+"</a> ("+NameOfMonth+" "+parseInt(PostDays[EntryNum],10)+", "+PostYears[EntryNum]+")<br />");
+ NameOfMonth = MonthNames[parseInt(PostMonths[EntryNum],10)-1]
+ document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+"</a> <span style=\'font-size:10px\'>// "+parseInt(PostDays[EntryNum],10)+" "+NameOfMonth+", "+PostYears[EntryNum]+"</span><br />");
     }
 }
